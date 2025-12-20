7c90de435138760f9ea737698abd921311a2636c
ekagra <ekagrashharma@gmail.com>
2025-12-20 21:46:33 +0530

    frontend code setup

---
+++ back02/Backend/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+
+const app= express()
+
+app.get('/' , (req,res)=>{
+    res.send('hello world');
+});
+
+const PORT = process.env.PORT | 3000 ;
+
+app.listen(PORT, ()=>{
+    console.log(`Server at http://localhost:${PORT}`);
+    }
+)
