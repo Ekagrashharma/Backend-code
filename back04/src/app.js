3fb1af3a5f8099fd082732f625e7e1bb00945d58
ekagra <ekagrashharma@gmail.com>
2025-12-26 14:51:11 +0530

    router add error in postman

--- back04/src/app.js
+++ back04/src/app.js
@@ -3,21 +3,20 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true   
-  })
-);
 
-app.use(express.json({
-    limit:"16kb"
-}))
+  app.use(cors({origin: process.env.CORS_ORIGIN,credentials: true,}));
+  app.use(express.json({limit: "16kb",}));
+  app.use(express.urlencoded({extended: true,limit: "16kb",}));
+  app.use(express.static("public"));
+  app.use(cookieParser());
 
-app.use(express.urlencoded({
-    extended:true , limit:"16kb"
-}))
+  // routes import 
+  import userRouter from "./routes/user.routes.js";
 
-app.use(express.static("public"))
+  // routes decleration 
+  app.use("api/v1/users", userRouter)
+
+  // http:/localhost:8000/users/registor
+
 
 export { app };
